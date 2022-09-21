70e51fb5d304774c91ba55c6752fa32852c8d5fc
BlazeofTerra <102171681+BlazeofTerra@users.noreply.github.com>
2022-09-21 12:08:46 +0100

    consolidated css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './css/App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import PostCreate from "./pages/postCreate"
