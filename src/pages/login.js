bcbc9c91c062ad653d5699329623b2899b12a63d
emmawojcik <71179439+emmawojcik@users.noreply.github.com>
2022-09-22 10:38:54 +0100

    login page functionality, not styled yet

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,22 +1,32 @@
-import React from "react"
+import React, { useState } from "react"
 import '../css/login.css';
-import Navbar from '../components/TopNavbar'
+import { login } from '../utils';
 
-function Login() {
-    return (
-      <>
-        <Navbar />
+const Login = ({setter}) => {
 
-        <div>
-          <form>
-            <label>Username</label>
-            <input></input>
-          </form>
+  const [username, setUsername] = useState()
+  const [password, setPassword] = useState()
 
-        </div>        
-      </>
+  const submitHandler = async (event) => {
+    event.preventDefault()
+    await login(username, password, setter)
+  }
 
-    )
+  return (
+    <div id="logInContainer">
+
+      <form onSubmit = {submitHandler} id="loginForm">
+        <h1>Login</h1>
+        <input onChange={(event) => setUsername(event.target.value)} placeholder="Username:"/>
+
+        <input onChange={(event) => setPassword(event.target.value)} placeholder="Password:"/>
+
+        <button type="submit">Log In</button>
+    
+      </form> 
+
+    </div>
+  )
 }
 
 export default Login
