84921969984db44bf18074b78665da5f7dd6b060
emmawojcik <71179439+emmawojcik@users.noreply.github.com>
2022-09-23 15:16:50 +0100

    improved widths of form

--- src/pages/login.js
+++ src/pages/login.js
@@ -14,17 +14,19 @@ const Login = ({setter}) => {
 
   return (
     <div id="logInContainer">
+      <h1>SIGN IN</h1>
 
       <form onSubmit = {submitHandler} id="loginForm">
-        <h1>SIGN IN</h1>
+        
         <input onChange={(event) => setUsername(event.target.value)} placeholder="Username" class="loginInput"/>
 
         <input onChange={(event) => setPassword(event.target.value)} placeholder="Password" class="loginInput"/>
 
         <button type="submit">SIGN IN</button>
-    
+
       </form> 
 
+      <h2>Not a member? Create Account</h2>
     </div>
   )
 }
