14a438b474d450bf1c8821c48b41de60de3d9ffe
Hmullla <hannahmulla4@gmail.com>
2022-09-26 15:12:38 +0100

    added icons

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import '../css/profile.css';
 import { deleteUser } from "../utils";
+import { MdSaveAlt, MdLibraryAdd } from "react-icons/md";
+import { AiTwotoneEdit } from "react-icons/ai";
 
 function Profile({loggedIn, setLoggedIn, user, email}) {
     const navigate = useNavigate();
@@ -22,6 +24,10 @@ function Profile({loggedIn, setLoggedIn, user, email}) {
         console.log("logged out")
         await setLoggedIn(false)
         await navigate("/login")
+    }
+
+    const listPage = async () => {
+        await navigate("/postCreate")
     }
     
 
@@ -47,11 +53,11 @@ function Profile({loggedIn, setLoggedIn, user, email}) {
                     <h3>{email}</h3>
                     <h3>location</h3>
                 </div>
-                <h1 className="bio-title">Bio</h1>
+                <h1 className="bio-title">Bio <button className="save"><MdSaveAlt/></button> <button className="edit"><AiTwotoneEdit/></button></h1>
                 <div class="bio">
                 <input type="text" class="bio__input" placeholder="Enter a bio"/>
                 </div>
-                <h1 className="items-title">My Items</h1>
+                <h1 className="items-title">My Items <button className="add" onClick={() => listPage()}><MdLibraryAdd/></button></h1>
                 <div class="list-items">
                 <input type="text" class="list-items__input" placeholder="You currently do not have any items for sale"/>
                 </div>
