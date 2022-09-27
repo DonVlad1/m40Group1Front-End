541d5fb13e15085f3c12097e106f26613e4a466c
zellski1234 <zellski1234@gmail.com>
2022-09-27 16:34:28 +0100

    updated navbar css

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,6 @@ function Home() {
     // test comment
     return (
         <div id="homeContent">
-            <img src='./images/carlogo.png' alt=''/>
             <h1>All about the drive</h1>
             <h2>Find the car that is right for your journey</h2>
             <div className='btn'>
