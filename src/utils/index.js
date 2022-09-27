95eaa176225c747d0a13c82ed61998ef0f82bb64
BlazeofTerra <102171681+BlazeofTerra@users.noreply.github.com>
2022-09-27 11:06:35 +0100

    added list posts to the utils/index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -146,6 +146,21 @@ export const listUsers = async (setter) =>
     }
 }
 
+export const listPosts = async (setter) => {
+    try {
+        const response = await fetch("http://localhost:5000/posts", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+        return response.json()
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
 // ----------------------------- update -----------------------------
 
 export const updateName = async ( token, username ) => {
